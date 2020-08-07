6884dd7710c4a2f07a00284b32355aea54ae0368
MoeinSorkhei <m.moein.sorkhei@gmail.com>
2020-08-07 09:37:27 +0200

    scripts added to .gitignore

--- scripts/others/clean_midgard.sh
+++
@@ -1,7 +0,0 @@
-python3 main.py --clean_midgard --dataset cityscapes --model c_glow_v2 --direction label2photo --cond_mode segment
-python3 main.py --clean_midgard --dataset cityscapes --model c_glow_v2 --direction photo2label
-
-python3 main.py --clean_midgard --dataset cityscapes --model c_glow_v3 --direction label2photo --cond_mode segment
-
-python3 main.py --clean_midgard --dataset maps --model c_flow --direction map2photo
-python3 main.py --clean_midgard --dataset maps --model c_flow --direction photo2map

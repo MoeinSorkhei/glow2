57fe35647f9cef7ecbe18d90b6e6a9327ff3d0d1
MoeinSorkhei <m.moein.sorkhei@gmail.com>
2020-08-14 09:48:58 +0200

    ActNorm Function works well
    Initial grad check for ActNormFunction computes the gradients correctly. It may be needed to improve the grad computation to make it more efficient. Also in the final code the unneeded tensors should be freed inside forward and backward.

--- src/models/glow/actnorm.py
+++ src/models/glow/actnorm.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+from torch.autograd import grad
 
 from .cond_net import ActCondNet, compute_batch_stats
 
@@ -64,3 +65,70 @@ class ActNorm(nn.Module):
     def reverse(self, out):
         return (out / self.scale) - self.loc
 
+
+class ActNormNoMemory(nn.Module):
+    def __init__(self, in_channel):
+        super().__init__()
+
+        self.loc = nn.Parameter(torch.zeros(1, in_channel, 1, 1))  # this operation is done channel-wise
+        self.scale = nn.Parameter(torch.ones(1, in_channel, 1, 1))  # loc, scale: vectors applied to all channels
+        self.register_buffer('initialized', torch.tensor(0, dtype=torch.uint8))
+        self.activations = []
+
+    def initialize(self, inp):
+        mean, std = compute_batch_stats(inp)
+        # data dependent initialization
+        self.loc.data.copy_(-mean)
+        self.scale.data.copy_(1 / (std + 1e-6))
+
+    def forward(self, inp):
+        # data-dependent initialization of scale and shift
+        if self.initialized.item() == 0:  # to be initialized the first time
+            self.initialize(inp)
+            self.initialized.fill_(1)
+
+        # forward without storing activations
+        return ActNormFunction.apply(inp, self.loc, self.scale, self.activations)
+
+    def reverse(self, out):
+        return (out / self.scale) - self.loc
+
+
+class ActNormFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, inp, loc, scale):
+        with torch.no_grad():  # compute output without forming computational graph
+            _, _, height, width = inp.shape  # input of shape [bsize, in_channel, h, w]
+            logdet = height * width * torch.sum(logabs(scale))
+            output = scale * (inp + loc)
+            # TODO: delete the tensors
+
+        ctx.save_for_backward(loc, scale)
+        ctx.output = output
+        return output, logdet
+
+    @staticmethod
+    def backward(ctx, grad_output, grad_logdet):
+        loc, scale = ctx.saved_tensors
+        output = ctx.output
+
+        with torch.no_grad():  # apply the inverse of the operation
+            reconstructed = (output / scale) - loc
+            reconstructed.requires_grad = True  # so we can compute grad for this
+
+        with torch.enable_grad():  # creating computational graph and compute gradients
+            output = scale * (reconstructed + loc)
+            _, _, height, width = reconstructed.shape  # input of shape [bsize, in_channel, h, w]
+            logdet = height * width * torch.sum(logabs(scale))
+            output.retain_grad()
+            # compute grad for loc
+            grad_loc = grad(outputs=output, inputs=loc, grad_outputs=grad_output, retain_graph=True)[0]
+            # compute grad for scale
+            grad_scale_output = grad(outputs=output, inputs=scale, grad_outputs=grad_output, retain_graph=True)[0]
+            grad_scale_logdet = grad(outputs=logdet, inputs=scale, grad_outputs=grad_logdet, retain_graph=True)[0]
+            grad_scale = grad_scale_output + grad_scale_logdet
+            # compute grad for inp
+            grad_inp = grad(outputs=output, inputs=reconstructed, grad_outputs=grad_output, retain_graph=True)[0]
+
+        # TODO: delete the tensors - are gradients effecient? (computation)
+        return grad_inp, grad_loc, grad_scale
